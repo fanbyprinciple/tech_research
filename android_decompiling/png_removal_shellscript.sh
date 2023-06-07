e621181377c1e149daa874ae424325fa026c9538
fanbyprinciple <fanbyprinciple>
2023-06-07 06:27:31 -0400

    adding android repos

---
+++ android_decompiling/png_removal_shellscript.sh
@@ -0,0 +1,11 @@
+rm -rf /home/kali/tech_research/android_decompiling/Sandes_2.1.3_Apkpure/res/drawable-mdpi/people_28_filled.png
+cp /home/kali/tech_research/android_decompiling/Sandes_2.1.3_Apkpure/res/drawable-mdpi/person_add_24_filled.png /home/kali/tech_research/android_decompiling/Sandes_2.1.3_Apkpure/res/drawable-mdpi/people_28_filled.png
+
+rm -rf /home/kali/tech_research/android_decompiling/Sandes_2.1.3_Apkpure/res/drawable-hdpi/people_28_filled.png
+cp /home/kali/tech_research/android_decompiling/Sandes_2.1.3_Apkpure/res/drawable-hdpi/person_add_24_filled.png /home/kali/tech_research/android_decompiling/Sandes_2.1.3_Apkpure/res/drawable-hdpi/people_28_filled.png
+
+rm -rf /home/kali/tech_research/android_decompiling/Sandes_2.1.3_Apkpure/res/drawable-xxhdpi/people_28_filled.png
+cp /home/kali/tech_research/android_decompiling/Sandes_2.1.3_Apkpure/res/drawable-xxhdpi/person_add_24_filled.png /home/kali/tech_research/android_decompiling/Sandes_2.1.3_Apkpure/res/drawable-xxhdpi/people_28_filled.png
+
+rm -rf /home/kali/tech_research/android_decompiling/Sandes_2.1.3_Apkpure/res/drawable-xxxhdpi/people_28_filled.png
+cp /home/kali/tech_research/android_decompiling/Sandes_2.1.3_Apkpure/res/drawable-xxxhdpi/person_add_24_filled.png /home/kali/tech_research/android_decompiling/Sandes_2.1.3_Apkpure/res/drawable-xxxhdpi/people_28_filled.png
