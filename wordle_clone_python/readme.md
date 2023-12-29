35ea0b5022da5d7e9f756b82c909a5cc70211f0d
fanbyprinciple <fanbyprinciple@gmail.com>
2023-12-29 11:06:53 +0530

    pokewordle test 1

--- wordle_clone_python/readme.md
+++ wordle_clone_python/readme.md
@@ -1,2 +1,8 @@
 # creating wordle clone with python
 
+1. trying to pass first doc test
+
+    python -m doctest -v wordle_clone.py
+
+    ![](20231229110626.png)
+    
--- wordle_clone_python/wordle_clone.py
+++ wordle_clone_python/wordle_clone.py
@@ -1,27 +1,38 @@
 import pathlib
 import random
 
-pathlib.Path('wordlist.txt').read_text(encoding="utf-8")
 
-all_poke = pathlib.Path("wordlist.txt")
+def get_random_word():
 
-WORDLIST = [
-    word.upper() for word in all_poke.read_text(encoding="utf-8").strip().split("\n") if len(word) == 7
-]
+    pathlib.Path('wordlist.txt').read_text(encoding="utf-8")
 
-# print(all_words)
+    all_poke = pathlib.Path("wordlist.txt")
 
-word = random.choice(WORDLIST)
 
-for guess in range(1,7):
-    guess = input(f"Guess {guess}: ").upper()
-    if guess == word:
-        print("correct")
-        break
+    WORDLIST = [
+        word.upper() for word in all_poke.read_text(encoding="utf-8").strip().split("\n") if len(word) == 7
+    ]
+
+    # print(all_words)
+
+    word = random.choice(WORDLIST)
+    return word
+
+def show_guess(guess, word):
+
+    """Show the user's guess on the terminal and classify all letters.
+
+    ## Example:
+
+    >>> show_guess("CRANE", "SNAKE")
+    Correct letters: A, E
+    Misplaced letters: N
+    Wrong letters: C, R
+    """
+
     correct_letters = {
-        letter for letter, correct in zip(guess, word) if letter == correct
-    }
-
+            letter for letter, correct in zip(guess, word) if letter == correct
+        }
     misplaced_letters = set(guess) & set(word) - correct_letters
     wrong_letters = set(guess) - set(word)
 
@@ -29,5 +40,23 @@ for guess in range(1,7):
     print("Misplaced letters:", ", ".join(sorted(misplaced_letters)))
     print("Wrong letters:", ", ".join(sorted(wrong_letters)))
 
-else:
+def game_over(word):
     print(f"The word was {word}")
+
+
+def main():
+
+    word = get_random_word()    
+
+    for guess in range(1,7):
+        guess = input(f"Guess {guess}: ").upper()
+        show_guess(guess, word)
+        if guess == word:
+            print("correct")
+            break    
+
+    else:
+        game_over(word)
+
+if __name__ == "__main__":
+    main()
